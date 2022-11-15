1c5d46cb5f51eb3f8c78794c36053a9250cd792f
afekTheMiniLearner <afeksa2000@gmail.com>
2022-11-15 16:22:48 +0200

    added hover only to mini like of SIGN UP NOW on the login page

--- client/scripts/recipes.js
+++ client/scripts/recipes.js
@@ -1,35 +1,4 @@
-let recipes = [
-    {
-        _id: '631379106b61d9484ecea744',
-        name: 'mac-and-cheese',
-        creator: 'anonymous',
-        ingredients: 'pasta,cheese,mushrooms,salt,pepper',
-        cookingTime: '01:15',
-        difficulityLevel: 'medium',
-        createdAt: new Date('2022-09-03T15:56:00.232+0000'),
-        updatedAt: new Date('2022-09-03T15:56:00.232+0000'),
-    },
-    {
-        _id: '6313883afd1a3a0ecc264d75',
-        name: 'spicy-mio-sauce',
-        creator: 'anonymous',
-        ingredients: 'mayo,black-pepper,chilli-powder',
-        cookingTime: '00:10',
-        difficulityLevel: 'easy',
-        createdAt: new Date('2022-09-03T17:00:42.527+0000'),
-        updatedAt: new Date('2022-09-03T17:00:42.527+0000'),
-    },
-    {
-        _id: '634c5dfd38f770946df89192',
-        name: 'steak-angos',
-        creator: 'arnold-schwarzeneggerr',
-        ingredients: 'angos-meat, olive-oil, salt, black-pepper',
-        cookingTime: '01:45',
-        difficulityLevel: 'easy',
-        createdAt: new Date('2022-10-16T19:39:41.697+0000'),
-        updatedAt: new Date('2022-10-16T19:39:41.697+0000'),
-    },
-];
+let recipes = [];
 
 function fetchRecipes() {
     return new Promise((resolve, reject) => {
